b4f387c507e04fab3f8852b7b3fc0464625c7d94
M7moudAbdallah01 <mahmoudcs50course@gmail.com>
2025-08-09 15:23:08 +0300

    Create course.php

---
+++ course.php
@@ -0,0 +1,23 @@
+<?php include('navbar.php'); ?>
+<?php include('db.php'); ?>
+
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>تفاصيل الكورس</title>
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/course.css">
+</head>
+<body>
+
+<main>
+    <h2>تفاصيل الكورس</h2>
+    <!-- هنا هيظهر تفاصيل الكورس بناءً على الـ ID -->
+</main>
+
+<?php include('footer.php'); ?>
+<script src="../js/app.js"></script>
+</body>
+</html>
