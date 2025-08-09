428b32fab4a510042636295585e8e38473fb52e7
M7moudAbdallah01 <mahmoudcs50course@gmail.com>
2025-08-09 15:23:10 +0300

    Create courses.php

---
+++ courses.php
@@ -0,0 +1,23 @@
+<?php include('navbar.php'); ?>
+<?php include('db.php'); ?>
+
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>كورسات السنة الدراسية</title>
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/courses.css">
+</head>
+<body>
+
+<main>
+    <h2>كورسات السنة الدراسية</h2>
+    <!-- هنا هيظهر الكورسات بناءً على قاعدة البيانات -->
+</main>
+
+<?php include('footer.php'); ?>
+<script src="../js/app.js"></script>
+</body>
+</html>
