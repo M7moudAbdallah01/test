c5185d7091207447751143bf948b8c30c5476225
M7moudAbdallah01 <mahmoudcs50course@gmail.com>
2025-08-10 01:38:33 +0300

    ..................

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 function toggleMenu() {
-  const nav = document.getElementById("navLinks");
-  if (nav) {
-    nav.style.display = nav.style.display === "flex" ? "none" : "flex";
-  }
+    const navLinks = document.getElementById("navLinks");
+    const overlay = document.getElementById("overlay");
+
+    navLinks.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+function closeMenu() {
+    document.getElementById("navLinks").classList.remove("active");
+    document.getElementById("overlay").classList.remove("active");
 }
