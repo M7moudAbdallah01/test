e96cbcb6cf32f17ea714a533480aa92d8d7e2081
M7moudAbdallah01 <mahmoudcs50course@gmail.com>
2025-08-10 02:36:02 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,3 +10,17 @@ function closeMenu() {
     document.getElementById("navLinks").classList.remove("active");
     document.getElementById("overlay").classList.remove("active");
 }
+
+const backToTopButton = document.getElementById("Backtotop");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 300) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
